69c42620416a80658d32322dd6ec37a61828cb58
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-05-20 22:14:11 -0500

    got local env up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,8 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
-                </a>
+             		Getting Local Dev Env up   
+	    </a>
             </header>
         </div>
     );
