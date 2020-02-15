73c6a3fd7f3acd190b801b93764b9a8bceee330e
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2020-02-15 15:39:46 -0600

    adding hot relead

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+test("it assesses the true", () => {
+  expect(1 + 1).toEqual(2);
+});
+
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/meow/i);
